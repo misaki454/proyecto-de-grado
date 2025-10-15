d8c9c26bc3c082ba0277f40e07077d0c770a53db
misaki454 <perezanabel454@gmail.com>
2025-10-15 07:25:04 -0400

    SCENES

---
+++ Proyectodegrado/Assets/PausarJuego.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PausarJuego : MonoBehaviour
+{
+    public GameObject MenuPausa;
+    public bool juegoPausado = false;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (juegoPausado)
+            {
+                Reanudar();
+
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+    }
+    public void Reanudar()
+    {
+        MenuPausa.SetActive(false);
+        Time.timeScale = 1;
+        juegoPausado = false;
+
+    }
+    public void Pausar()
+    {
+        MenuPausa.SetActive(true);
+        Time.timeScale = 0;
+        juegoPausado = true;
+    }
+    
+
+}
+
+ 
+
